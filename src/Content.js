c64580ed8a1d78434d129de799fb56ab61346485
Your Name <arteevraina@gmail.com>
2020-01-06 22:25:52 +0530

    update

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,33 @@
 import React from "react"
+import axios from "axios"
 
-const Content = () => {
-    return(
-        <div>
-            <h1>Content</h1>
-        </div>
-    )
+class Content extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            articles: []
+        }
+    }
+
+    componentDidMount() {
+    const apiPath = "https://newsapi.org/v2/top-headlines?"
+    const country = "country=us&"
+    const apiKey = "apiKey=10ec0e1a10374ee88678d00ea394efce"
+    const url = apiPath + country + apiKey
+    axios.get(url)
+        .then(response => {
+            console.log(response.data.articles)
+            this.setState({articles: response.data.articles})
+        })
+        
+    }
+
+    render() {
+        return(
+            <p>Hello World</p>
+        )
+    }
 }
 
 export default Content
