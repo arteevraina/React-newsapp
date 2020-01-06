eaa58f17ccb2535fead3fdd4c5ac107dcee79100
Your Name <arteevraina@gmail.com>
2020-01-06 19:18:32 +0530

    addition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
 import Header from "./Header"
+import Content from "./Content"
 
 const App = () => {
   return(
-    <h1><Header/></h1>
+    <div>
+      <h1><Header/></h1>
+      <Content/>
+    </div>
   )
 }
 
