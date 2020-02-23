7d36cfab90aafdde77619e299f98259bd2d8d50d
Your Name <arteevraina@gmail.com>
2020-02-23 12:09:18 +0530

    Updates

---
+++ precache-manifest.6c8b9f8ee7139a540a9d756204d260aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053d7e482b7e3e36d020d50cb6136c46",
+    "url": "/React-newsapp/index.html"
+  },
+  {
+    "revision": "7becc2a9b4e21b06e690",
+    "url": "/React-newsapp/static/css/main.75c4f0d8.chunk.css"
+  },
+  {
+    "revision": "01b6844e4018a489fc23",
+    "url": "/React-newsapp/static/js/2.72139fb3.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/React-newsapp/static/js/2.72139fb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7becc2a9b4e21b06e690",
+    "url": "/React-newsapp/static/js/main.7cff9f31.chunk.js"
+  },
+  {
+    "revision": "60791d40ed5a9fa79509",
+    "url": "/React-newsapp/static/js/runtime-main.a90bef4e.js"
+  }
+]);
